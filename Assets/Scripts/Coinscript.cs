09b2119436e526713117e7374473100429a8f8cc
29-CheongChinWei <115056022+29-CheongChinWei@users.noreply.github.com>
2022-10-11 13:21:28 +0800

    added game win and lose scene

---
+++ Assets/Scripts/Coinscript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coinscript : MonoBehaviour
+
+{
+    public int score;
+    public GameObject scoreTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreTxt.GetComponent<Text>().text = "Score :" + score;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            score += 10;
+            Destroy(collision.gameObject);
+        }
+    }
+}
